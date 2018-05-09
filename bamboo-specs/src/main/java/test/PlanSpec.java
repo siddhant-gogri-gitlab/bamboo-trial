6294464c8b08768e326562537e1d9ad4da37c986
Siddhant Gogri <siddhant.gogri@addteq.com>
2018-05-09 18:12:35 -0400

    Initial commit

---
+++ bamboo-specs/src/main/java/test/PlanSpec.java
@@ -0,0 +1,62 @@
+import com.atlassian.bamboo.specs.api.BambooSpec;
+import com.atlassian.bamboo.specs.api.builders.BambooKey;
+import com.atlassian.bamboo.specs.api.builders.BambooOid;
+import com.atlassian.bamboo.specs.api.builders.permission.PermissionType;
+import com.atlassian.bamboo.specs.api.builders.permission.Permissions;
+import com.atlassian.bamboo.specs.api.builders.permission.PlanPermissions;
+import com.atlassian.bamboo.specs.api.builders.plan.Job;
+import com.atlassian.bamboo.specs.api.builders.plan.Plan;
+import com.atlassian.bamboo.specs.api.builders.plan.PlanIdentifier;
+import com.atlassian.bamboo.specs.api.builders.plan.Stage;
+import com.atlassian.bamboo.specs.api.builders.plan.branches.BranchCleanup;
+import com.atlassian.bamboo.specs.api.builders.plan.branches.PlanBranchManagement;
+import com.atlassian.bamboo.specs.api.builders.plan.configuration.ConcurrentBuilds;
+import com.atlassian.bamboo.specs.api.builders.project.Project;
+import com.atlassian.bamboo.specs.builders.task.ScriptTask;
+import com.atlassian.bamboo.specs.builders.trigger.RepositoryPollingTrigger;
+import com.atlassian.bamboo.specs.util.BambooServer;
+
+@BambooSpec
+public class PlanSpec {
+    
+    public Plan plan() {
+        final Plan plan = new Plan(new Project()
+                .oid(new BambooOid("1ept99rtawkxt"))
+                .key(new BambooKey("TEST"))
+                .name("Test"),
+            "Test",
+            new BambooKey("TEST"))
+            .oid(new BambooOid("1epjk26g32sjl"))
+            .pluginConfigurations(new ConcurrentBuilds()
+                    .useSystemWideDefault(false))
+            .stages(new Stage("Default Stage")
+                    .jobs(new Job("Default Job",
+                            new BambooKey("JOB1"))
+                            .tasks(new ScriptTask()
+                                    .inlineBody("echo \"Working\""))))
+            .triggers(new RepositoryPollingTrigger())
+            .planBranchManagement(new PlanBranchManagement()
+                    .delete(new BranchCleanup())
+                    .notificationForCommitters());
+        return plan;
+    }
+    
+    public PlanPermissions planPermission() {
+        final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("TEST", "TEST"))
+            .permissions(new Permissions()
+                    .userPermissions("root", PermissionType.EDIT, PermissionType.VIEW, PermissionType.ADMIN, PermissionType.CLONE, PermissionType.BUILD));
+        return planPermission;
+    }
+    
+    public static void main(String... argv) {
+        //By default credentials are read from the '.credentials' file.
+        BambooServer bambooServer = new BambooServer("http://159.203.82.192:8085");
+        final PlanSpec planSpec = new PlanSpec();
+        
+        final Plan plan = planSpec.plan();
+        bambooServer.publish(plan);
+        
+        final PlanPermissions planPermission = planSpec.planPermission();
+        bambooServer.publish(planPermission);
+    }
+}
